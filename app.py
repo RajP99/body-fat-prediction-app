d984efe1230b6f425ebe6592de4a080f0be0fb1a
Raj Parikh <38576505+RajP99@users.noreply.github.com>
2022-03-12 00:42:57 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify, request
 import pickle
 import os
 import traceback
+import pandas as pd
 import numpy as np
 from sklearn.preprocessing import StandardScaler
 
@@ -17,7 +18,14 @@ def predict():
 		json = request.get_json()	 
 		temp=list(json[0].values())
 		temp = np.array([temp])
+		
+		dataset=pd.read_csv("bodyfat.csv")
+		dataset=dataset.drop(["BodyFat", "Density"],axis=1)
+		X=dataset.values
+		
 		SC=StandardScaler()
+		X=SC.fit_transform(X)
+
 		temp = SC.transform(temp)
 		rfr = pickle.load(open('rfr.pkl', 'rb'))
 		prediction = rfr.predict(temp)
